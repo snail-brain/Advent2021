1194db34d400ffe799c8cd223c50766faa64307c
snail-brain <snail-brain@protonmail.com>
2021-12-06 16:49:35 -0600

    day 4 part 1

---
+++ Day4/pt1.py
@@ -0,0 +1,101 @@
+import numpy as np
+
+
+rnd = open("rndInput.txt", "r")
+boards = open("boards.txt", "r")
+
+
+# Create array of the given numbers
+rndIn = ""
+for number in rnd:
+    rndIn = number
+rndArray = rndIn.split(",")
+
+# Create array of given game boards
+hitMap = []
+boardsArray = []
+skip = 5
+count = 0
+
+for number in boards:
+    # Skip empty lines
+    if count == skip:
+        skip = skip + 5
+    else:
+        # Remove extra spaces then add next array into board list
+        edits = ((number.strip()).replace("  ", " ")).split(" ")
+        boardsArray.append(edits)
+        hitMap.append([0, 0, 0, 0, 0])
+        count += 1
+
+# Modify both hitmap and boards to be numpy 3d arrays
+x = np.array(boardsArray).reshape(100, 5, 5)
+newHitMap = np.array(hitMap).reshape(100, 5, 5)
+
+
+def drawing(rnd):
+    # For every number drawn...
+    for drawing in rnd:
+        boardCount = -1
+        while boardCount < len(x) - 1:
+            boardCount += 1
+            rowCount = -1
+            # Comb through each row of the current board
+            while rowCount < 4:
+                rowCount += 1
+                columnCount = -1
+                # Check if any number in each row equals the number drawn
+                while columnCount < 4:
+                    columnCount += 1
+                    # If said number does equal the number drawn...
+                    if int(x[boardCount, rowCount, columnCount]) == int(drawing):
+                        # Mark on hitmap then check for bingo
+                        newHitMap[boardCount, rowCount, columnCount] = 1
+                        checkWin, winner = checkBingo()
+                        # If there is a bingo...
+                        # Return the winning board and the winning number
+                        if checkWin:
+                            return winner[0], int(drawing)
+
+
+def checkBingo():
+    i = 0
+    # iterate through entire hitmap looking for a whole column or row to return true
+    # return the winning row or column
+    while i < 100:
+        y = 0
+        x = 0
+        while y < 5:
+            if all(newHitMap[i, y, :]):
+                return True, [i, y, "y"]
+            y += 1
+        while x < 5:
+            if all(newHitMap[i, :, x]):
+                return True, [i, x, "x"]
+            x += 1
+        i += 1
+    return False, 0
+
+
+def endingCalculations(winningBoard, winningNum):
+    sum = 0
+    y = 0
+
+    # iterate through the winning board
+    # add up the sum of all unmarked spaces in said board
+    while y < 5:
+        z = 0
+        while z < 5:
+            if newHitMap[winningBoard, y, z] == 0:
+                sum += int(x[winningBoard, y, z])
+            z += 1
+        y += 1
+    return sum * winningNum
+
+
+def main():
+    winningBoard, winningNum = drawing(rndArray)
+    print(endingCalculations(winningBoard, winningNum))
+
+
+main()
