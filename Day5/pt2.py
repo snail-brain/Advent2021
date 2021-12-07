2ff56cf1e46639571dada0517ffa2930ed33d1d7
snail-brain <snail-brain@protonmail.com>
2021-12-07 17:48:20 -0600

    day 5 part 2

--- Day5/pt2.py
+++ Day5/pt2.py
@@ -16,15 +16,12 @@ def findLarge():
     i = 0
     largeX = 0
     largeY = 0
-
-    # Iterate thorugh each coordinate pair
     while i < len(newLines):
         x = 0
-        # Iterate through each coordinate from each pair
         while x < 2:
             checkX = int(newLines[i, x, 0])
             checkY = int(newLines[i, x, 1])
-            # Find the highest point so we know how large to make our grid
+
             if checkX > largeX:
                 largeX = checkX
             if checkY > largeY:
@@ -36,7 +33,7 @@ def findLarge():
 
 largeX, largeY = findLarge()
 
-# Make the graph we'll be charting our lines on
+
 for num in range(largeX * largeY):
     lineGraph.append(0)
 newGraph = (np.array(lineGraph)).reshape(largeX, largeY)
@@ -44,18 +41,18 @@ newGraph = (np.array(lineGraph)).reshape(largeX, largeY)
 
 def checkPlot():
 
-    # Iterate through each coordinate pair
     for tit in newLines:
         x1 = int(tit[0, 0])
         x2 = int(tit[1, 0])
         y1 = int(tit[0, 1])
         y2 = int(tit[1, 1])
 
-        # If we find a horizontal or veritcal line, plot it
         if x1 == x2:
             plotX(x1, y1, y2)
-        if y1 == y2:
+        elif y1 == y2:
             plotY(x1, x2, y1)
+        else:
+            plotDiag(x1, x2, y1, y2)
 
 
 def plotX(x, y1, y2):
@@ -78,6 +75,35 @@ def plotY(x1, x2, y):
         while x1 <= x2:
             newGraph[x1, y] += 1
             x1 += 1
+
+
+def plotDiag(x1, x2, y1, y2):
+    # Four different directions a diagonal can go
+
+    # Up left
+    if x1 > x2 and y1 > y2:
+        while x1 >= x2 and y1 >= y2:
+            newGraph[x1, y1] += 1
+            x1 -= 1
+            y1 -= 1
+    # Up right
+    elif x1 < x2 and y1 > y2:
+        while x1 <= x2 and y1 >= y2:
+            newGraph[x1, y1] += 1
+            x1 += 1
+            y1 -= 1
+    # Down left
+    elif x1 > x2 and y1 < y2:
+        while x1 >= x2 and y1 <= y2:
+            newGraph[x1, y1] += 1
+            x1 -= 1
+            y1 += 1
+    # Down right
+    elif x1 < x2 and y1 < y2:
+        while x1 <= x2 and y1 <= y2:
+            newGraph[x1, y1] += 1
+            x1 += 1
+            y1 += 1
 
 
 def twoLines():
